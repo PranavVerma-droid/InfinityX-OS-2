102cea62692c960ff766631cd38b712fdd4866b6
Pranav Verma <pranav@verma.net.in>
2025-02-24 11:57:35 +0530

    Update: Updated Bootloader.

--- src/bootloader.asm
+++ src/bootloader.asm
@@ -1,11 +1,20 @@
-mov ah, 0x0e
-mov al, 'A'
+[org 0x7c00]            ; Tell assembler where this code will be loaded (lul)
 
-loop:
-    inc al
-    int 0x10
-    jmp loop
+; Set up segments (FUCK)
+mov ax, 0               ; Initialize segments to 0
+mov ds, ax
+mov es, ax
+mov ss, ax
+mov sp, 0x7c00         ; Stack grows downwards from where we are loaded
 
+; Print a character
+mov ah, 0x0e           ; BIOS teletype output
+mov al, 'H'            ; Character to print
+int 0x10               ; Print it
+
+; Infinite loop
 jmp $
-times 510-($-$$) db 0
-db 0x55, 0xaa
+
+; Boot sector padding
+times 510-($-$$) db 0  ; Fill the rest of sector with zeros
+dw 0xaa55             ; Boot signature at the end of bootloader
